d447102ca0541ecec54e010c9ea02ef1557699a3
Nerto Kageh <kagehnerto@yahoo.com>
2023-01-19 14:58:28 -0600

    Initial Commit

---
+++ Country Flag Game/Models/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Country Flag Game
+//
+//  Created by Induran Aderinto  on 1/19/23.
+//
+
+import Foundation
+
+struct Answer: Identifiable {
+    struct Question: Identifiable {
+        var id: UUID()
+        var correctAnswer: Answer
+        var incorrectAnswer: [Answer]
+    }
+    var id = UUID()
+    var text: String
+    var isCorrect: Bool
+}
